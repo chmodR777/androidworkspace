adee3a5b3bbc4da4d3d59ac5ad0d216e46251e43
“tangke” <16377168@qq.com>
2026-03-01 13:30:19 +0800

    App 模块调用 SDK 的 add 方法 5.1 导入 AAR 文件到 App 模块 在 App 模块（app）的libs目录下（如果没有则新建），将第四步生成的addsdk-release.aar复制进去。 打开 App 模块的build.gradle（Module 级），在dependencies节点下添加依赖：
    5.2 在 App 中调用 add 方法
    修改 App 模块的MainActivity.java，调用 SDK 的 add 方法：
    
    5.3 修改布局文件（显示结果）
    打开 App 模块的res/layout/activity_main.xml，添加一个 TextView：

--- JNIAddSDK/app/src/main/java/com/example/jniaddsdk/MainActivity.java
+++ JNIAddSDK/app/src/main/java/com/example/jniaddsdk/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.jniaddsdk;
 
+import com.example.addsdk.NativeAddUtils;
+
 import android.os.Bundle;
 
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
+
+    // 定义TAG常量，用于日志标记
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        // 1. 获取TextView用于显示结果
+        TextView tvResult = findViewById(R.id.tv_result);
+
+        // 2. 调用SDK的add方法
+        int a = 10;
+        int b = 50000;
+        // 方式1：创建对象调用
+        NativeAddUtils addUtils = new NativeAddUtils();
+        int result1 = addUtils.add(a, b);
+        // 方式2：调用静态封装方法
+        int result2 = NativeAddUtils.calculateAdd(a, b);
+
+        // 3. 打印日志并显示结果
+        Log.d(TAG, "a=" + a + ", b=" + b + ", 相加结果1：" + result1);
+        Log.d(TAG, "a=" + a + ", b=" + b + ", 相加结果2：" + result2);
+        tvResult.setText("10 + 20 = " + result1);
+
+
     }
 }
